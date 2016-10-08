2e8420252603e3ce9393577b0436570409553297
huangziyi01 <huangziyi01@baidu.com>
2016-10-08 16:45:58 +0800

    quick push

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import Radio from './radio'
 import RadioGroup from './radio-group'
-import RadioButton from './radio-button'
 
 export default Radio
-export { Radio, RadioGroup, RadioButton }
+export { Radio, RadioGroup }
